6f4af5a82b5c357e7ff41e930f85b41108bccf6c
ZarechOleksii <69851647+ZarechOleksii@users.noreply.github.com>
2020-09-19 13:34:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 
-def task_six(n):
+def find_kaprekar(amount):
     to_answer = []
     x = 1
-    while len(to_answer) != n:
+    while len(to_answer) != amount:
         for y in range(1, len(str((x ** 2))) + 1):
             if (((x ** 2) % (10 ** y)) + ((x ** 2) // (10 ** y)) == x) and ((x ** 2) % (10 ** y) != 0):
                 to_answer.append(x)
@@ -11,6 +11,11 @@ def task_six(n):
     return to_answer
 
 
-print('\nEnter the quantity of Kaprekar numbers:')
-taskSixN = int(input())
-print('First ', taskSixN, ' Kaprekar numbers are: ', task_six(taskSixN))
+try:
+    print('\nEnter the quantity of Kaprekar numbers:')
+    quantity = int(input())
+    if quantity < 0:
+        raise ValueError
+    print('First ', quantity, ' Kaprekar numbers are: ', find_kaprekar(quantity))
+except ValueError:
+    print('Wrong input, enter a positive integer')
