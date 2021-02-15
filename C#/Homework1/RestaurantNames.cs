aad32fd4538bb2559ab353671526d8c5e7420a1e
ZarechOleksii <misterz06022002@gmail.com>
2021-02-15 02:42:28 +0200

    Homework1

---
+++ C#/Homework1/RestaurantNames.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework1
+{
+    public enum RestaurantNames
+    {
+        Delice = 1,
+        EuroHotel = 2,
+        Morio = 3
+        
+    }
+}
