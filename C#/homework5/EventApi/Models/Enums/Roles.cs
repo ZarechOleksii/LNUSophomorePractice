5146db7174b7d12490ba325df4902a92c2ce811f
ZarechOleksii <69851647+ZarechOleksii@users.noreply.github.com>
2021-04-05 08:17:42 +0300

    Add files via upload

---
+++ C#/homework5/EventApi/Models/Enums/Roles.cs
@@ -0,0 +1,16 @@
+﻿using NpgsqlTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventApi
+{
+    public enum Roles
+    {
+        [PgName("User")]
+        User,
+        [PgName("Admin")]
+        Admin
+    }
+}
---
+++ C#/homework5/EventApi/Services/IOrderService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventApi.Services
+{
+    interface IOrderService
+    {
+        public object GetAll();
+        public object GetOrdersOfUser(string email);
+        public object GetOrderById(string email, int id);
+        public object AddOrder(string email, RequestModels.NewOrder newOrder);
+        public object GetOrderByIdAdmin(int id);
+    }
+}
